c26bbd89619a9044d6b6c07a0b3df19af4b1419b
Scott Gress <scottmgress@gmail.com>
2018-01-14 22:38:25 -0600

    Add ability to export Lambda functions for generic events
    This allows writing machines that will be run by cron jobs (i.e. cloudwatch events) or any other type of event source that Lambda supports.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const _ = require('@sailshq/lodash');
 const rttc = require('rttc');
-const async = require('async');
-const machineAsAction = require('machine-as-action');
-const statuses = require('statuses');
+const packageForHttpEvent = require('./lib/http-event');
+const packageForGenericEvent = require('./lib/generic-event');
 
 module.exports = function machineAsLambda(optsOrMachineDefOrMachine, options) {
 
@@ -57,208 +56,16 @@ module.exports = function machineAsLambda(optsOrMachineDefOrMachine, options) {
 
   // Default options.
   options = options || {};
+  _.defaults(options, {
+    eventType: 'http'
+  });
 
-  // MAAify the passed-in machine (or machine def), unless it's already been done.
-  let actionMachine = optsOrMachineDefOrMachine.IS_MACHINE_AS_ACTION ? optsOrMachineDefOrMachine : machineAsAction(optsOrMachineDefOrMachine);
-
-  // Wrap it in a function that expects Lambda arguments, and mocks req/res out of them.
-  return function(event, context, callback) {
-
-    // Attempt to parse the body, or else leave it as "null".
-    let body = null;
-    try {
-      body = JSON.parse(event.body)
-    }
-    catch (e) {
-      // no-op.
-    }
-
-    try {
-
-      //  ███████╗███████╗████████╗██╗   ██╗██████╗
-      //  ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
-      //  ███████╗█████╗     ██║   ██║   ██║██████╔╝
-      //  ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝
-      //  ███████║███████╗   ██║   ╚██████╔╝██║
-      //  ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
-      //
-
-      // Keep track of headers set in the machine.
-      let headers = {};
-
-      // Keep track of the status code set by the machine.
-      let statusCode = 200;
-
-      // Keep a hash of lower-cased headers for `req.get`.
-      let _lcHeaders = Object.keys(event.headers || {}).reduce((memo, header) => { memo[header.toLowerCase()] = event.headers[header]; return memo }, {});
-
-      // Mock up the `req` that the action will use.
-      const req = {
-        param: (key) => {
-          return (event.pathParameters && event.pathParameters[key]) || (body && body[key]) || (event.queryStringParameters && event.queryStringParameters[key]) || undefined;
-        },
-        query: event.queryStringParameters,
-        method: event.httpMethod,
-        path: event.path,
-        headers: event.headers,
-        get: (header, defaultVal) => _lcHeaders[header.toLowerCase()] || defaultVal,
-        authorizationToken: event.authorizationToken,
-        awsEvent: event,
-        awsContext: context
-      };
-
-      // Mock up the `res` that the action will use.
-      const res = {
-        set: (header, value) => { headers[header] = value; return res; },
-        status: (statusCode) => { statusCode = statusCode; return res; },
-        json: (output) => {
-          headers['Content-Type'] = 'application/json';
-          if (!options.noEnvelope) {
-            output = JSON.stringify(output);
-          }
-          respond(output);
-        },
-        send: (output) => {
-          let payload = output;
-          if (typeof payload !== 'number' && typeof payload !== 'boolean' && payload !== null) {
-            if (typeof payload === 'string') {
-              headers['Content-Type'] = 'text/html';
-            }
-            else {
-              headers['Content-Type'] = 'application/json';
-            }
-            if (!options.noEnvelope) {
-              payload = JSON.stringify(payload);
-            }
-          }
-          respond(payload)
-        },
-        sendStatus: (statusCode) => respond(statuses(statusCode), statusCode),
-        serverError: (output) => {
-          return res.json(output.stack, 500);
-        },
-        badRequest: (output) => {
-          return res.sendStatus(400);
-        },
-        forbidden: (output) => {
-          return res.sendStatus(403);
-        },
-        notFound: (output) => {
-          return res.sendStatus(404);
-        },
-        // Allow setting context for AWS response (mainly for use in authorizers).
-        context: {}
-      }
-
-      // Helper function to send a valid response for a Lambda function using the API Gateway Proxy.
-      const respond = (body, _statusCode, _headers) => {
-
-        // Set the status code for the response.
-        if (_statusCode) { statusCode = _statusCode; }
-
-        // Set the headers for the response.
-        if (_headers) { Object.assign(headers, _headers ); }
-        // If a content-length header wasn't added manually, put one in now based on the body length.
-        if (!(headers['Content-Length'])) {
-          headers['Content-Length'] = body.toString().length;
-        }
-        // Add CORS headers if necessary.
-        if (options.cors && (options.cors.origin === '*' || event.headers['Origin'] === options.cors.origin)) {
-          headers['Access-Control-Allow-Origin'] = options.cors.origin;
-          headers['Access-Control-Allow-Headers'] = options.cors.headers && (typeof options.cors.headers === 'string' ? options.cors.headers : options.cors.headers.join(','));
-          headers['Access-Control-Allow-Credentials'] = options.cors.allowCredentials;
-        }
-
-        // If a teardown option was specified, run the teardown before sending the response.
-        if (typeof options.teardown === 'function') {
-          options.teardown = [options.teardown]
-        }
-        if (_.isArray(options.teardown) && options.teardown.length) {
-          return async.series(options.teardown, () => {
-            if (options.noEnvelope) {
-              return callback(null, body);
-            }
-            callback(null, {
-              statusCode: statusCode,
-              headers: headers,
-              body: body,
-              context: res.awsContext
-            });
-          })
-        }
-
-        // Otherwise just send the response.
-        if (options.noEnvelope) {
-          return callback(null, body);
-        }
-        callback(null, {
-          statusCode: statusCode,
-          headers: headers,
-          body: body,
-          context: res.awsContext
-        });
-      }
-
-      //  ██████╗ ██╗   ██╗███╗   ██╗     █████╗  ██████╗████████╗██╗ ██████╗ ███╗   ██╗
-      //  ██╔══██╗██║   ██║████╗  ██║    ██╔══██╗██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║
-      //  ██████╔╝██║   ██║██╔██╗ ██║    ███████║██║        ██║   ██║██║   ██║██╔██╗ ██║
-      //  ██╔══██╗██║   ██║██║╚██╗██║    ██╔══██║██║        ██║   ██║██║   ██║██║╚██╗██║
-      //  ██║  ██║╚██████╔╝██║ ╚████║    ██║  ██║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║
-      //  ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝    ╚═╝  ╚═╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
-      //
-
-      // If there's a bootstrap, run that first.
-      if (typeof options.bootstrap === 'function') {
-        options.bootstrap = [options.bootstrap]
-      }
-      if (_.isArray(options.bootstrap) && options.bootstrap.length) {
-        async.series(options.bootstrap, (err) => {
-          if (err) {
-            if (options.noEnvelope) {
-              return callback(null, {
-                event: event,
-                error: err.stack
-              })
-            }
-            callback(null,{
-              statusCode: 500,
-              headers: {},
-              body: JSON.stringify({
-                event: event,
-                error: err.stack
-              })
-            });
-            return;
-          }
-          // Run the machine after a successful return from the bootstrap.
-          return actionMachine(req, res);
-        });
-        return;
-      }
-
-      // Run the machine action with the mocked req and res.
-      return actionMachine(req, res);
-    }
-
-    // Most errors should be caught by the machine runner and directed to the `error` exit
-    // of the machine.  This will catch errors in machine-as-lambda itself.
-    catch (e) {
-      if (options.noEnvelope) {
-        return callback(null, {
-          event: event,
-          error: err.stack
-        })
-      }
-      callback(null,{
-        statusCode: 500,
-        headers: {},
-        body: JSON.stringify({
-          event: event,
-          error: e.stack
-        })
-      });
-    }
-
+  if (options.eventType === 'http') {
+    return packageForHttpEvent(optsOrMachineDefOrMachine, options);
   }
+
+  return packageForGenericEvent(optsOrMachineDefOrMachine, options);
+
+
 
 };
